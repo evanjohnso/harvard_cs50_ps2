244459d12fe0411903914f264b4222a8fc21592b
Evan Johnson <evanrjohnso@gmail.com>
2018-01-02 05:01:59 +0000

    command line argument validation logic

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -5,17 +5,17 @@
 #include <stdlib.h>
 #include <math.h>
 
-int ALPHA_SIZE = 26;
 void encrypt(string s, int key);
 void shift_char(char c, int key, int ascii);
+bool valid_cmd_args(int argc, string argv[]);
 
 int main(int argc, string argv[])
 {
-    if (argc < 2)
+    if (!valid_cmd_args(argc, argv))
     {
-        printf("Missing the encryption key as second argument!\n");
         return -1;
     }
+
     printf("plaintext: ");
     string s = get_string();
     printf("cyphertext: ");
@@ -52,7 +52,25 @@ void shift_char(char c, int key, int ascii)
     // subtract ascii # to create 0-25 range
     int alpha_index = c - ascii;
     // shift by key (% is net..1, 27, 54 all shift by 2)
-    char cypher_char = (alpha_index + key) % ALPHA_SIZE + ascii;
+    char cypher_char = (alpha_index + key) + ascii;
     // add the ascii amount back to retain proper case
     printf("%c", cypher_char);
+}
+
+bool valid_cmd_args(int argc, string argv[])
+{
+    if (argc < 2 || argc > 2)
+    {
+        printf("Invalid command line arguments.\nPlease enter an encryption string as only argument.\n");
+        return false;
+    }
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if (!isalpha(argv[1][i]))
+        {
+            printf("Please enter a valid encryption key\n");
+            return false;
+        }
+    }
+    return true;
 }
